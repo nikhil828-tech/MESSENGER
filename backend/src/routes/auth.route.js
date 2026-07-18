c0fba6384681b7781bddee41e04e2d2b490b13f7
Nikhil <nicksramsoni@gmail.com>
2026-07-18 21:55:53 +0530

    initial commit

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+const router = express.Router();
+
+router.post("/signup", (req, res) => {
+  // Handle user signup logic here
+  res.send("Signup endpoint");
+});
+
+router.get("/login", (req, res) => {
+  res.send("Login endpoint");
+});
+
+router.get("/logout", (req, res) => {
+  res.send("Logout endpoint");
+});
+
+export default router;
