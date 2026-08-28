13f1bf0162ed33a99cb0a1b50f05013442d60b70
Nikhil <nicksramsoni@gmail.com>
2026-08-28 17:53:21 +0530

    welcome eamil done

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,10 +2,15 @@ import dns from "node:dns";
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 import mongoose from "mongoose";
+import { ENV } from "./env.js";
 
 export const connectDB = async () =>{
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const {MONGO_URI} = ENV;
+        if(!MONGO_URI){
+            throw new Error("MONGO_URI is not defined in the environment variables");
+        }
+        const conn = await mongoose.connect(ENV.MONGO_URI);
         console.log("MONGODB CONNECTED",conn.connection.host);
         
     }catch (error){
