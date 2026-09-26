306e1966e3c9acfe63fef97f5dd8c98de90267bb
Nikhil <nicksramsoni@gmail.com>
2026-09-26 07:22:32 +0530

    login frontend implementede

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,8 @@ export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp:false,
-
+    isLoggingIn:false,
+    isLogginOut:false,
     checkAuth: async () => {
         try {
             const res = await ax.get("/auth/check");
@@ -31,5 +32,32 @@ export const useAuthStore = create((set) => ({
         }finally {
             set({isSigningUp:false});
         }
+    },
+    login: async (data) =>{ 
+        set({isLoggingIn:true});
+        try{
+            const res = await ax.post("/auth/login",data);
+            set({authUser : res.data});
+
+            //toast
+            toast.success("Logged in successfully");
+        } catch (error){
+            toast.error(error.response.data.message);
+        }finally {
+            set({isLoggingIn:false});
+        }
+    },
+    logout: async () => {
+        set({isLogginOut:true});
+        try{
+            await ax.post("/auth/logout");
+            toast.success("Logged out successfully");
+            set({authUser:null});
+            
+        } catch (error){
+            toast.error("error logging out");
+        }
+        
+        
     }
 }));
